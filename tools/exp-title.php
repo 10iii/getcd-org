367842adfbf3979303cee2ac3fc3ac0e69658071
10iii <10iiishisan@gmail.com>
2012-11-12 22:14:24 +0800

    exp title

---
+++ tools/exp-title.php
@@ -0,0 +1,13 @@
+﻿<?php
+//echo "\n";
+require_once("./config.php");
+$conn = new PDO("mysql:host=$db_host;dbname=$db_dbname", $db_username, $db_password , array(PDO::ATTR_PERSISTENT => true));
+$conn->query("SET NAMES utf8");
+$sqlstr = "SELECT topic_id, title FROM gcd_topic LIMIT 1000";
+//echo $sqlstr."\n";
+$rows = $conn->query($sqlstr);
+echo $rows->rowCount()."\n";
+foreach ($rows as $row) {
+	echo str_pad($row["topic_id"], 30).preg_replace("/\s/", " ", $row["title"])."\n";
+}
+?>
