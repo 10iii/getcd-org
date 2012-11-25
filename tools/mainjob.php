977fe7e0e0c66912ba0c8622c36ca46b1b671e1c
10iii <10iiishisan@gmail.com>
2012-11-25 11:21:32 +0800

    echo in mainjob.php

--- tools/mainjob.php
+++ tools/mainjob.php
@@ -6,31 +6,31 @@ function logout ($logstr) {
 }
 chdir("/www/getcd-org/tools");
 logout("main job start.");
-shell_exec("cd /www/getcd-org/tools");
+echo shell_exec("cd /www/getcd-org/tools");
 
 logout("start to fetch SC index.");
-shell_exec("node g-sc-index.js > sc-ind-log~ 2>&1");
+echo shell_exec("node g-sc-index.js > sc-ind-log~ 2>&1");
 logout("finished fetch SC index.");
 
 logout("start to fetch SC detail.");
-shell_exec("node g-sc-detail.js > sc-det-log~ 2>&1");
+echo shell_exec("node g-sc-detail.js > sc-det-log~ 2>&1");
 logout("finished fetch SC detail.");
 
 logout("start to fetch SC thumb.");
-shell_exec("php g-sc-thumb.php > sc-thb-log~ 2>&1");
+echo shell_exec("php g-sc-thumb.php > sc-thb-log~ 2>&1");
 logout("finished fetch SC thumb.");
 
 logout("start to execute sql daily job.");
-shell_exec("/opt/lampp/bin/mysql -ugetcddb -pgetat0408 -e'source dailysql.sql' getcd");
+echo shell_exec("/opt/lampp/bin/mysql -ugetcddb -pgetat0408 -e'source dailysql.sql' getcd");
 logout("finished execute sql daily job.");
 
 logout("start to copy thumb to img server.");
-shell_exec("cp -u -R fetched/thumb /www/img-getcd-org/ ");
+echo shell_exec("cp -u -R fetched/thumb /www/img-getcd-org/ ");
 logout("finished copy thumb");
 
 
 logout("start to seg title for search.");
-shell_exec("php exp-title.php |./wordseg |php rep-title-seg.php");
+echo shell_exec("php exp-title.php |./wordseg |php rep-title-seg.php");
 logout("finished seg title");
 
 
